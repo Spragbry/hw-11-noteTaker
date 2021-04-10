a6bd23379f230292181cf79533f5047e880505d0
Bryce Spraggins <spragbry93@gmail.com>
2021-04-10 09:40:40 -0400

    final add ons

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const { title } = require('process')
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const path = require('path')
 
 app.get('/notes', function (req, res) {
